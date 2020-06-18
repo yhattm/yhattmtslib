766d309e44b1a6afce7400106ad24579d3cf3c84
Ben <ben@vivotek.com>
2020-06-18 05:49:24 +0000

    add GetOption

--- src/modules/invest.ts
+++ src/modules/invest.ts
@@ -1,7 +1,18 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
-const getStock = async () => {
+const getConfig = async () => {
+  const res = await fetch(
+    'https://docs.google.com/document/export?format=txt&id=1SWsU4Dv93oqN67IuVAa9W8Qh75GeNWgA8C3MUKMOfLQ&token=AC4w5VgI0vCcwM4qgBYm5QHFEE3upEWAgQ%3A1540802477358&includes_info_params=true'
+  );
+  const text = await res.text();
+  const cutText = text.substring(1, text.length);
+  const jsonObject = JSON.parse(cutText);
+  return jsonObject;
+};
+
+const GetStock = async () => {
+  console.log(getConfig);
   const res = await fetch(
     'https://tw.screener.finance.yahoo.net/future/aa03?fumr=futurepart&opmr=optionpart&random=0.6297693371261759'
   );
@@ -23,5 +34,31 @@ const getStock = async () => {
   return price;
 };
 
-const invest = { getStock };
+const GetOption = async (query: { date: string; target: string }) => {
+  const { date, target } = query;
+  const res = await fetch(
+    'https://tw.screener.finance.yahoo.net/future/aa03?fumr=futurepart&opmr=optionpart&opym=' +
+      date +
+      '&random=0.6297693371261759'
+  );
+  const body = await res.text();
+  const $ = cheerio.load(body);
+  const callPrice = $("td:contains('" + target + "')")
+    .prev()
+    .prev()
+    .prev()
+    .prev()
+    .prev();
+  const putPrice = $("td:contains('" + target + "')")
+    .next()
+    .next()
+    .next();
+  const result = {
+    call: parseFloat($(callPrice).text()),
+    put: parseFloat($(putPrice).text()),
+  };
+  return result;
+};
+
+const invest = { GetStock, GetOption };
 export default invest;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,16 @@
 import { invest } from '../src';
 
-describe('index', () => {
-  it('invest', async () => {
-    const res = await invest.getStock();
+describe('invest', () => {
+  it('GetStock', async () => {
+    const res = await invest.GetStock();
     console.log(res);
     expect(res.FIMTX1).toBeGreaterThan(0);
+  });
+  it('GetOption', async () => {
+    const res = await invest.GetOption({ date: '202006', target: '11500' });
+    console.log(res);
+    if (res) {
+      expect(res.call).toBeGreaterThan(0);
+    }
   });
 });
