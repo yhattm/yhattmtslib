44b6a9011066f22b481d6143b7579b55ee13166b
yhattm <yhattm@gmail.com>
2022-09-20 21:32:02 +0800

    fix export GPIO

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import invest from './modules/invest';
 import gas from './modules/gas';
 import token from './modules/token';
-import gpio from './gpio'
+import { GPIO } from './gpio'
 
-export { invest, gas, token, gpio };
+export { invest, gas, token, GPIO };
