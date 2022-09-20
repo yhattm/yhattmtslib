2a833669547ed8bdeecc7b1a91dcb3ce8928ca9f
yhattm <yhattm@gmail.com>
2022-09-20 21:27:01 +0800

    release to add gpio

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import invest from './modules/invest';
 import gas from './modules/gas';
 import token from './modules/token';
+import gpio from './gpio'
 
-export { invest, gas, token };
+export { invest, gas, token, gpio };
